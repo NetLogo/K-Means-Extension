685992fc94432e706844f43fc775b44e5e17b321
nicolaspayette <nicolaspayette@gmail.com>
2013-08-14 18:20:40 -0400

    add basic exception handling

--- src/main/scala/org/nlogo/extensions/kmeans/KMeansExtension.scala
+++ src/main/scala/org/nlogo/extensions/kmeans/KMeansExtension.scala
@@ -1,18 +1,18 @@
 package org.nlogo.extensions.kmeans
 
 import scala.collection.JavaConverters._
-
 import org.nlogo.api.AgentSet
 import org.nlogo.api.Argument
 import org.nlogo.api.Context
 import org.nlogo.api.DefaultClassManager
 import org.nlogo.api.DefaultReporter
+import org.nlogo.api.ExtensionException
 import org.nlogo.api.PrimitiveManager
 import org.nlogo.api.ScalaConversions.toLogoList
 import org.nlogo.api.Syntax._
 import org.nlogo.api.Turtle
-
 import edu.uci.ics.jung.algorithms.util.KMeansClusterer
+import edu.uci.ics.jung.algorithms.util.KMeansClusterer.NotEnoughClustersException
 
 class KMeansExtension extends DefaultClassManager {
   def load(primitiveManager: PrimitiveManager) {
@@ -25,12 +25,17 @@ object KMeansClustersPrim extends DefaultReporter {
     Array(AgentsetType, NumberType, NumberType, NumberType),
     ListType)
   override def report(args: Array[Argument], context: Context) =
+    try {
       toLogoList(KMeans.clusters(
         args(0).getAgentSet, // turtles
         args(1).getIntValue, // nbClusters
         args(2).getIntValue, // maxIterations
         args(3).getDoubleValue, // convergenceThreshold
         context.getRNG))
+    } catch {
+      case e: java.lang.IllegalArgumentException => throw new ExtensionException(e)
+      case e: NotEnoughClustersException         => throw new ExtensionException(e)
+    }
 }
 
 object KMeans {
